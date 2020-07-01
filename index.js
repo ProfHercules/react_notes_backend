6082841a9794dd3cfe00fbc4c1c17969b88ca4f5
Hercules <hercosmith03@gmail.com>
2020-07-01 20:22:47 +0200

    Added error-handling & more endpoints

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
+app.use(express.static("build"));
+app.use(express.json());
 require("dotenv").config();
 const express = require("express");
 const Note = require("./models/note");
 const cors = require("cors");
 const app = express();
 app.use(cors());
-app.use(express.json());
-app.use(express.static("build"));
+
+app.get("/api/notes", (request, response) => {
+  Note.find({}).then((notes) => {
+    response.json(notes);
+  });
+});
 
 app.post("/api/notes", (request, response) => {
   const body = request.body;
@@ -25,29 +31,56 @@ app.post("/api/notes", (request, response) => {
   });
 });
 
-app.get("/api/notes", (request, response) => {
-  Note.find({}).then((notes) => {
-    response.json(notes);
-  });
-});
-
 app.get("/api/notes/:id", (request, response) => {
-  Note.findById(request.params.id).then((note) => {
-    response.json(note);
-  });
+  Note.findById(request.params.id)
+    .then((note) => {
+      if (note) {
+        response.json(note);
+      } else {
+        response.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 
-app.delete("/api/notes/:id", (request, response) => {
-  const id = Number(request.params.id);
-  notes = notes.filter((note) => note.id !== id);
+app.put("/api/notes/:id", (request, response, next) => {
+  const body = request.body;
 
-  response.status(204).end();
+  const note = {
+    content: body.content,
+    important: body.important,
+  };
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then((updatedNote) => {
+      response.json(updatedNote);
+    })
+    .catch((error) => next(error));
+});
+
+app.delete("/api/notes/:id", (request, response, next) => {
+  Note.findByIdAndRemove(request.params.id)
+    .then((result) => {
+      response.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
 const unknownEndpoint = (_, response) => {
   response.status(404).send({ error: "unknown endpoint" });
 };
 app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "Malformatted Id" });
+  }
+
+  next(error);
+};
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
