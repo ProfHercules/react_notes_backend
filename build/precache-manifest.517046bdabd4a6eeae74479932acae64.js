b64d787d24472a5280ac241b5c4d0330f113c63e
Hercules <hercosmith03@gmail.com>
2020-07-01 13:46:21 +0200

    uibuild

---
+++ build/precache-manifest.517046bdabd4a6eeae74479932acae64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79a1fbe4ea1710540ea05cad4f0dd3e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4049f67044caf42c9a25",
+    "url": "/static/css/main.f69d1026.chunk.css"
+  },
+  {
+    "revision": "07ad9742558be04874c1",
+    "url": "/static/js/2.1a65c4d2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1a65c4d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4049f67044caf42c9a25",
+    "url": "/static/js/main.68a228a0.chunk.js"
+  },
+  {
+    "revision": "50dafbbe6ac08ccc90c7",
+    "url": "/static/js/runtime-main.e19f6162.js"
+  }
+]);
