16bc944a7fff1293cced9a8382a014152dff41ae
Hercules <hercosmith03@gmail.com>
2020-07-02 20:11:32 +0200

    Improved test readability

--- controllers/notes.js
+++ controllers/notes.js
@@ -6,7 +6,7 @@ notesRouter.get("/", async (request, response) => {
   response.json(notes.map((note) => note.toJSON()));
 });
 
-notesRouter.get("/:id", async (request, response, next) => {
+notesRouter.get("/:id", async (request, response) => {
   const note = await Note.findById(request.params.id);
 
   if (note) {
@@ -16,7 +16,7 @@ notesRouter.get("/:id", async (request, response, next) => {
   }
 });
 
-notesRouter.post("/", async (request, response, next) => {
+notesRouter.post("/", async (request, response) => {
   const body = request.body;
 
   const note = new Note({
