521677527c693873be3dd844428013fb45d46392
Hercules <hercosmith03@gmail.com>
2020-07-02 08:23:26 +0200

    Refactor project structure

--- index.js
+++ index.js
@@ -1,92 +1,10 @@
-require("dotenv").config();
-const express = require("express");
-const Note = require("./models/note");
-const cors = require("cors");
-const app = express();
-app.use(express.static("build"));
-app.use(express.json());
-app.use(cors());
+const app = require("./app"); // varsinainen Express-sovellus
+const http = require("http");
+const config = require("./utils/config");
+const logger = require("./utils/logger");
 
-app.get("/api/notes", (request, response) => {
-  Note.find({}).then((notes) => {
-    response.json(notes);
-  });
-});
+const server = http.createServer(app);
 
-app.post("/api/notes", (request, response, next) => {
-  const body = request.body;
-
-  if (body.content === undefined) {
-    return response.status(400).json({ error: "Content missing!" });
-  }
-
-  const note = new Note({
-    content: body.content,
-    important: body.important || false,
-    date: new Date(),
-  });
-
-  note
-    .save()
-    .then((savedNote) => savedNote.toJSON())
-    .then((formattedNote) => response.json(formattedNote))
-    .catch((error) => next(error));
-});
-
-app.get("/api/notes/:id", (request, response, next) => {
-  Note.findById(request.params.id)
-    .then((note) => {
-      if (note) {
-        response.json(note);
-      } else {
-        response.status(404).end();
-      }
-    })
-    .catch((error) => next(error));
-});
-
-app.put("/api/notes/:id", (request, response, next) => {
-  const body = request.body;
-
-  const note = {
-    content: body.content,
-    important: body.important,
-  };
-
-  Note.findByIdAndUpdate(request.params.id, note, { new: true })
-    .then((updatedNote) => {
-      response.json(updatedNote);
-    })
-    .catch((error) => next(error));
-});
-
-app.delete("/api/notes/:id", (request, response, next) => {
-  Note.findByIdAndRemove(request.params.id)
-    .then(() => {
-      response.status(204).end();
-    })
-    .catch((error) => next(error));
-});
-
-const unknownEndpoint = (_, response) => {
-  response.status(404).send({ error: "unknown endpoint" });
-};
-app.use(unknownEndpoint);
-
-const errorHandler = (error, request, response, next) => {
-  console.error(error.message);
-
-  if (error.name === "CastError") {
-    return response.status(400).send({ error: "Malformatted Id" });
-  } else if (error.name === "ValidationError") {
-    return response.status(400).json({ error: error.message });
-  }
-
-  next(error);
-};
-app.use(errorHandler);
-
-const PORT = process.env.PORT;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`);
 });
